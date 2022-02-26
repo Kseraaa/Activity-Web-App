cc850f2f26af46021b2fc696c09e08bede058a2a
Pattarachai Boriboon <pwppowerpack@gmail.com>
2022-02-26 19:09:54 +0700

    update 26/02/2565

---
+++ client/src/components/footer/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div className="App">
+            <h1></h1>
+        </div>
+    )
+}
--- client/src/components/history/history-card/history-card-1.js
+++ client/src/components/history/history-card/history-card-1.js
@@ -7,26 +7,24 @@ import { CardActionArea } from '@mui/material';
 
 function HistoryCard1() {
     return (
-        <div>
-            <Card sx={{ maxWidth: 'auto' }} >
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image="/static/images/cards/contemplative-reptile.jpg"
-                        alt="Title History"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            กิจกรรมวิ่งผลัด
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            จัดขึ้นวันที่...<br/>จบวันที่...
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </div>
+        <Card sx={{ maxWidth: 'auto' }} >
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="his1"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        กิจกรรมวิ่งผลัด
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        จัดขึ้นวันที่...<br/>จบวันที่...
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     );
 }
 
