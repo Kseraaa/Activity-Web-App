a6721bb2039b32e9970a33477e45f46e9ac66b47
Nattapong Naksri <naksri07@gmail.com>
2022-02-26 18:36:38 +0700

    contact

--- client/src/components/contact/contact-card.js
+++ client/src/components/contact/contact-card.js
@@ -1,6 +1,6 @@
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
-import { Box, IconButton, Typography } from '@mui/material';
+import { Box, Card, IconButton, Typography } from '@mui/material';
 
 function ContactCard() {
     return(
@@ -28,27 +28,26 @@ function ContactCard() {
                     Phone:
                     098 7654321
                 </Typography>
-                </Box>
-                
-                <Card 
-                    sx={{ 
-                        display: 'flex',
-                        flexDirection:'column',
-                        width: '100%',
-                        position: 'relative',
-                        top: 30,
-                        left: '20%',
-                    }}
-                >
-                    <IconButton aria-label='phone' sx={{mx: 'auto'}}>
-                        <EmailIcon/>
-                    </IconButton>
-                    <Typography sx={{textAlign: 'center'}}>
-                    Email:<br />
-                    6410110xxx@psu.ac.th<br />
-                    </Typography>
-                </Card>
             </Box>
+            
+            <Box
+                sx={{ 
+                    display: 'flex',
+                    flexDirection:'column',
+                    width: '100%',
+                    position: 'relative',
+                    left: 660,
+                    top: 800,
+                }}
+            >
+                <IconButton aria-label='phone' sx={{mx: 'auto'}}>
+                    <PhoneIcon/>
+                </IconButton>
+                <Typography sx={{textAlign: 'center'}}>
+                    Email: 6410110xxx@psu.ac.th
+                </Typography>
+            </Box>
+        </Box> 
     )
 };
 
