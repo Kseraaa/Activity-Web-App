ec7470567ed61d4d9e9792f80ae576b9274f2a22
ธนภัทร ด้วงแก้ว <thanapat0918618713@gmail.com>
2022-02-27 18:03:00 +0700

    update history

--- client/src/components/history/history-display-area.js
+++ client/src/components/history/history-display-area.js
@@ -6,34 +6,37 @@ import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import HistoryCard4 from "./history-card/history-card-4";
+import { Container } from "@mui/material";
+
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'black' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(0),
   color: theme.palette.text.secondary,
 }));
 
+
 export default function RowAndColumnSpacing() {
     return (
-
-            <Grid container rowSpacing={ 4 } columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
-                <Grid item xs={3}>
-                <Item><HistoryCard1/></Item>
-                </Grid>
-                <Grid item xs={3}>
-                <Item><HistoryCard2/></Item>
-                </Grid>
-                <Grid item xs={3}>
-                <Item><HistoryCard3/></Item>
-                </Grid>
-                <Grid item xs={3}>
-                <Item><HistoryCard4/></Item>
-                </Grid>
-                <Grid item xs={3}>
-                <Item><HistoryCard4/></Item>
-                </Grid>
+      <Container maxWidth="auto">
+        <Grid container rowSpacing={ 4 } columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
+          <Grid item xs={3}>
+            <Item><HistoryCard1/></Item>
             </Grid>
-
+            <Grid item xs={3}>
+            <Item><HistoryCard2/></Item>
+            </Grid>
+            <Grid item xs={3}>
+            <Item><HistoryCard3/></Item>
+            </Grid>
+            <Grid item xs={3}>
+            <Item><HistoryCard4/></Item>
+            </Grid>
+            <Grid item xs={3}>
+            <Item><HistoryCard4/></Item>
+            </Grid>
+          </Grid>
+        </Container>
     );
 } 
