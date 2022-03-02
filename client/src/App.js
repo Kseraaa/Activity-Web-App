b8c53cb5e50bfa5ae35b8043e8637d92b86659ff
Pattarachai Boriboon <pwppowerpack@gmail.com>
2022-03-02 22:16:28 +0700

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
-import Contact from "./pages/contact";
 import History from './pages/history';
+import Contact from './pages/contact';
+import FooterBar from './components/footer/footer'
+
 
 function App(){
   return(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/home"/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/history" element={<History/>}/>
-      </Routes>
-    </BrowserRouter>
+      <div className="page-container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/home"/>}/>
+            <Route path="/home" element={<Home/>}/>
+            <Route path="/history" element={<History/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Routes>
+        </BrowserRouter>
+      <FooterBar/>
+      </div>
   );
 }
 
--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,9 +1,72 @@
+import { Container, Box } from '@mui/material';
 import React from 'react';
+import './footer.css'
+import Avatar from '@mui/material/Avatar';
 
-const Footer = () => {
+
+export default function FooterBar() {
+
+
     return(
-        <div className="App">
-            <h1></h1>
+        <div className="main-footer">
+            <div className='container'>
+                <div className='row'>
+                    <table width="100%" >
+                        <tr>
+                            {/* column1 */}
+                            <td>
+                            <Container maxWidth="auto">
+                                    <div className='col'>
+                                        <h3>Contact Us</h3>
+                                        <ul className='list-unstyled'>
+                                            <Box sx={{ flexGrow: 2, display: { xs: 'none', md: 'flex' } }}>
+                                                <Avatar
+                                                    sx={{ width: 70, height: 65 }}
+                                                    variant="square"
+                                                    src="/img/fancierlogo1.png"
+                                                />
+                                                <Avatar
+                                                    sx={{ width: 130, height: 65 }}
+                                                    variant=""
+                                                    src="/img/fancierlogo2.png"
+                                                />
+                                            </Box><br/>
+                                            <div>Fancier INC.</div>
+                                            <div>YEAR : 1 | GROUP : 01-03</div>
+                                            <div>Department of Computer Engineering</div>
+                                            <div>Prince of Songkla University</div>
+                                            <div>Songkla, Thailand</div>
+                                        </ul>
+                                    </div>
+                            </Container>
+                            </td>
+                            {/* column2 */}
+                            <td>
+                            <Container maxWidth="auto">
+                                <h2 sx={{ mx: "auto"}}>
+                                    อยากลอง อยากทำ</h2>
+                                <h2 sx={{ mx: "auto" }}>
+                                    เลือกกิจกรรมที่ Fancier</h2>
+                            </Container>
+                            </td>
+
+                            {/* column3 */}
+                            <td>
+                            <Container maxWidth="auto">
+                                <div className='col'>
+                                    <h3>About Us</h3>
+                                    <ul className='list-unstyled'>
+                                        <div>กิจกรรม</div>
+                                        <div>ประวัติการเข้าร่วมกิจกรรม</div>
+                                        <div>ติดต่อ</div>
+                                    </ul>
+                                </div>
+                            </Container>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
