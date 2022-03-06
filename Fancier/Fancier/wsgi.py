a3f037b29b7a7dde20180fa06fd15e8f280a87f2
Nattapong Naksri <naksri07@gmail.com>
2022-03-06 16:24:24 +0700

    nat startapp

--- Fancier/Fancier/wsgi.py
+++ Fancier/Fancier/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AR project.
+WSGI config for Fancier project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AR.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fancier.settings')
 
 application = get_wsgi_application()
