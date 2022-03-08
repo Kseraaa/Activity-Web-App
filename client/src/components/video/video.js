f0018d86f90305887a82f8aa3dd6a48e0aac69d4
Pattarachai Boriboon <pwppowerpack@gmail.com>
2022-03-08 11:32:56 +0700

    video

---
+++ client/src/components/video/video.js
@@ -0,0 +1,25 @@
+import ProjectClip from "../video/Project-Clip.mp4";
+
+function Video() {
+    return(
+        <video
+            autoPlay
+            loop
+            muted
+            style={{
+            position: "absolute",
+            width: "auto",
+            left: "50%",
+            top: "59%",
+            height: "auto",
+            objectFit: "cover",
+            transform: "translate(-50%, -50%)",
+            zIndex: "-1"
+            }}
+        >
+            <source src={ProjectClip} type="video/mp4" />
+        </video>
+    );
+}
+
+export default Video ;
