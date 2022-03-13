002379025e0d70c50e755868261c4929f0d3d9d1
ปฏิญญา ลิ่มเฮ่า <teoylimhao2@gmail.com>
2022-03-13 22:00:06 +0700

    fixed

--- Fancier/Fancier/urls.py
+++ Fancier/Fancier/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
