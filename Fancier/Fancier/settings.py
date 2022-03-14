481c7f87a2585aab458c8edbf9ed60d827ef48e6
ปฏิญญา ลิ่มเฮ่า <teoylimhao2@gmail.com>
2022-03-14 11:57:59 +0700

    Deleted wrong google login

--- Fancier/Fancier/settings.py
+++ Fancier/Fancier/settings.py
@@ -44,14 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
 
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
 ]
-
-SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -95,11 +88,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
-]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- Fancier/Fancier/urls.py
+++ Fancier/Fancier/urls.py
@@ -11,6 +11,5 @@ route.register("", ActivityCardView, basename='activitycardview')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(route.urls)),
-    path('accounts/', include('allauth.urls')),
     path('auth/', include('auth.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
