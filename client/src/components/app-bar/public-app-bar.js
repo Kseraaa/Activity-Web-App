85f3a497e6f97e0c0c917b8326926d9a0c1a95b4
ภัทรชัย บริบูรณ์ <pwppowerpack@gmail.com>
2022-03-21 21:32:47 +0700

    Color in login button

--- client/src/components/app-bar/public-app-bar.js
+++ client/src/components/app-bar/public-app-bar.js
@@ -22,13 +22,14 @@ document.body.className = "AnErrorHasOccured";
 
 const useStyles = makeStyles({
     root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      background: 'linear-gradient(45deg, rgba(0, 195, 255, 0.8), rgba(182, 62, 238, 0.8))',
       border: 0,
       borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      boxShadow: '0 3px 5px 2px rgba(25, 0, 255, 0.2)',
       color: 'white',
       height: 48,
       padding: '0 30px',
+      fontSize: '15px'
     },
   });
 
